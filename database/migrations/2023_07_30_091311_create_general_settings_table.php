7e0a4a78c6a68c2c6346654ca1b03e6d9315364a
NH Tarik <nhtarik.cse@gmail.com>
2023-07-30 15:23:18 +0600

    settings db migration 1 table update

---
+++ database/migrations/2023_07_30_091311_create_general_settings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('general_settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('app_name');
+            $table->string('footer_text');
+            $table->string('light_logo');
+            $table->string('dark_logo');
+            $table->string('favicon');
+            $table->foreignId('lang_id');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('about_school');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('general_settings');
+    }
+};
