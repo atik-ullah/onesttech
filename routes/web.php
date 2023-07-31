fddc6fbfe2645a608bb7f5443eddb69a4b3e606c
Atik Ullah Nasar <atikullah.art@gmail.com>
2023-07-31 10:26:20 +0600

    done

--- routes/web.php
+++ routes/web.php
@@ -16,35 +16,8 @@ Route::get('/', function () {
 //Hefajt
 
 Route::get('/create/student',[StudentController::class, 'create'])->name('create');
-<<<<<<< HEAD
+
 Route::get('/index/student',[StudentController::class, 'index'])->name('index');
-=======
+
 Route::get('/index/student',[StudentController::class, 'index'])->name('index');
 Route::post('/store/student',[StudentController::class, 'store'])->name('store');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
-//-----    End Staff Manager Routes    ----->
->>>>>>> ee8de7ff6616b582fec102cafaea0458e68370be
