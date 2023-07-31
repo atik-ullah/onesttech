1a503578c979588e5ef67782d4038f87f9c42508
NH Tarik <nhtarik.cse@gmail.com>
2023-07-31 09:34:03 +0600

    add department route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ Route::get('/create/student',[StudentController::class, 'create'])->name('create
 Route::get('/index/student',[StudentController::class, 'index'])->name('index');
 Route::post('/store/student',[StudentController::class, 'store'])->name('student.store');
 
+
+
+//-----    Staff Manager Routes    ----->
+
+Route::get('/department', [DepartmentController::class, 'showDept'])->name('showDept');
+
+Route::get('/department/create', [DepartmentController::class, 'createDeptForm'])->name('createDeptForm');
+
+Route::post('/department/create', [DepartmentController::class, 'createDept'])->name('createDept');
+ 
+
+//-----    End Staff Manager Routes    ----->
