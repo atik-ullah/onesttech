001242bc42015b0ff4b076860929a6d972f95b75
Hefajtur <hefajtmunshi@gmail.com>
2023-08-03 19:35:34 +0600

    update

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,18 @@
 use App\Http\Controllers\DepartmentController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
-<<<<<<< HEAD
+
 use App\Http\Controllers\GaurdianController;
 use App\Http\Controllers\ClassesController;
 use App\Http\Controllers\ShiftController;
 use App\Http\Controllers\SectionController;
 
-=======
+
 use App\Http\Controllers\fees\GroupController;
 use App\Http\Controllers\fees\TypeController;
 use App\Http\Controllers\fees\MasterController;
 use App\Http\Controllers\fees\AssignController;
->>>>>>> 610d78a7b468cfaafc476083759a9dbf94d785f6
+
 
 
 
@@ -33,7 +33,7 @@ Route::get('/edit/student/{id}',[StudentController::class, 'edit'])->name('edit'
 Route::post('/update/student/{id}',[StudentController::class, 'update'])->name('update');
 Route::get('/destroy/student/{id}',[StudentController::class, 'destroy'])->name('destroy');
 
-<<<<<<< HEAD
+
 Route::get('/disable/student',[StudentController::class, 'disable'])->name('disable');
 Route::get('/getSection/{id}',[StudentController::class,'section'])->name('section');
 
@@ -83,7 +83,7 @@ Route::get('/show/shift',[ShiftController::class, 'shiftShow'])->name('shiftShow
 Route::get('/edit/shift/{id}',[ShiftController::class, 'shiftEdit'])->name('shiftEdit');
 Route::post('/update/shift/{id}',[ShiftController::class, 'shiftUpdate'])->name('shiftUpdate');
 Route::get('/destroy/shift/{id}',[ShiftController::class, 'shiftDestroy'])->name('shiftDestroy');
-=======
+
 //Atik
 Route::get('/groups/index',[GroupController::class, 'index'])->name('group.index');
 Route::any('/groups/create',[GroupController::class, 'create']);
@@ -110,122 +110,6 @@ Route::any('/assign/delete/{id}',[AssignController::class,'destroy']);
 Route::get('/get-section-by-classId/{id}',[AssignController::class,'section']); //done
 Route::any('/assign/collect',[AssignController::class,'collect']);
 Route::any('/collectSearch',[AssignController::class,'searchCollect']);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
